a188cf2cd52377974af1ba5d8c11d40ccc1848aa
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-02 14:02:36 +0100

    refactor: initialize a driver in beforeEach

--- test/DriverShould.test.ts
+++ test/DriverShould.test.ts
@@ -1,17 +1,21 @@
 import Driver from "../src/Driver";
 
 describe("Driver should",  () => {
+    let driver: Driver;
+
+    beforeEach(() => {
+        driver = new Driver(0, [1, 2, 3, 4]);
+    });
+
     it("return the stop at the beginning of the journey", () => {
-        const driver = new Driver(0, [1]);
         const stop = driver.getStopAt(0);
 
         expect(stop).toEqual(1);
     });
 
     it("return the stop at certain minute", () => {
-        const driver = new Driver(0, [1]);
         const stop = driver.getStopAt(60);
 
-        expect(stop).toEqual(1);
+        expect(stop).toEqual(4);
     });
 });
